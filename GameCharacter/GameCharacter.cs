2a004a302d5e50baddd5ea54235e988a4e48f5ff
jinag8904 <jinag8904@gmail.com>
2026-02-24 09:56:00 +0900

    실습 - 클래스
    GameCharacter, ProductInfo

---
+++ GameCharacter/GameCharacter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Security;
+
+public class GameCharacter
+{
+    string Name;
+    int Level;
+    int MaxHP => Level * 100;
+    int CurrentHP;
+    int AttackPower => Level * 10;
+    bool IsAlive => (CurrentHP > 0);
+
+    public GameCharacter(string name, int level)
+    {
+        Name = name;
+        Level = level;
+        CurrentHP = MaxHP;
+    }
+
+    public void Attack(GameCharacter target)
+    {
+        target.TakeDamage(this.AttackPower);
+        Console.WriteLine($"{this.Name}이(가) {target.Name}에게 {this.AttackPower} 데미지를 입혔습니다!");
+    }
+
+    public void TakeDamage(int damage)
+    {
+        CurrentHP -= damage;
+    }
+
+    public void ShowStatus()
+    {
+        Console.WriteLine($"[{Name}] Lv.{Level} - HP: {CurrentHP}/{MaxHP}, 공격력: {AttackPower}");
+    }
+}
---
+++ ProductInfo/ProductInfo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class ProductInfo
+{
+    string Name;
+    int Price;
+    public double DiscountPercent;
+    double DiscountAmount => (DiscountPercent / 100) * Price;
+    double FInalPrice => Price - DiscountAmount;
+
+    public ProductInfo(string name, int price)
+    {
+        Name = name;
+        Price = price;
+        DiscountPercent = 0;
+    }
+
+    public ProductInfo(string name, int price, int discountPercent)
+    {
+        Name = name;
+        Price = price;
+        DiscountPercent = discountPercent;
+    }
+
+    public void PrintInfo()
+    {
+        Console.WriteLine($"[상품 정보] {Name} - 가격: {Price}원, 할인: {DiscountPercent}% (-{DiscountAmount}원), 최종가: {FInalPrice}원");
+    }
+}
